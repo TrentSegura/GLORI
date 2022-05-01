38603eef2bba85327fec546643797dbc3d5b78be
Trent Segura <trnt@me.com>
2022-05-01 12:03:58 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+const cell = document.querySelector(".carousel-cell");
+const carousel = document.querySelector(".main-carousel");
+
+const generateSlides = (data) => {
+  carousel.innerHTML = "";
+
+  data.forEach((item, index) => {
+    let slideDiv = document.createElement("div");
+    slideDiv.classList.add("carousel-cell");
+
+    if (item.file) {
+      let img = document.createElement("img");
+      img.src = "./assets/slides/small/" + item.file;
+      img.dataset.original = "./assets/slides/large/" + item.file;
+
+      slideDiv.appendChild(img);
+    }
+
+    if (item.quote) {
+      let container = document.createElement("div");
+      container.classList.add("carousel-container");
+      container.classList.add("snippit");
+
+      let para = document.createElement("p");
+      para.classList.add("carousel-quote");
+      para.innerHTML = `"${item.quote}"`;
+
+      let attribute = document.createElement("div");
+
+      let h2 = document.createElement("h2");
+      h2.innerHTML = `${item.name}, `;
+
+      let h3 = document.createElement("h3");
+      h3.innerHTML = item.role;
+
+      slideDiv.appendChild(container);
+      container.appendChild(para);
+      container.appendChild(attribute);
+      attribute.appendChild(h2);
+      attribute.appendChild(h3);
+    }
+
+    carousel.appendChild(slideDiv);
+  });
+};
+
+generateSlides(data);
+
+const elem = document.querySelector(".main-carousel");
+const flkty = new Flickity(elem, {
+  // options
+  contain: true,
+  pageDots: false,
+  wrapAround: true,
+});
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".carousel-cell img");
+const large = document.querySelector(".large-modal-image");
+
+previews.forEach((preview) => {
+  preview.addEventListener("click", () => {
+    modal.classList.add("open");
+    large.classList.add("open");
+
+    const originalSrc = preview.getAttribute("data-original");
+    large.src = originalSrc;
+  });
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    large.classList.remove("open");
+  }
+});
---
+++ slideData.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    file: "ss_01.jpg",
+  },
+  {
+    quote:
+      "The father of a 17 year old with a severe leg injury was so worried his son would end up getting into trouble because his injury kept him from finding a legitimate job. No surgeons in the Dominican Republic had been able to offer his son any options other than a cane.  Our team provided his son with the  surgical intervention needed for recovery. He wanted me to know how thankful he was for us coming there and helping.",
+    name: "Felicia",
+    role: "GLORI volunteer",
+  },
+  {
+    file: "ss_02.jpg",
+  },
+  {
+    file: "ss_03.jpg",
+  },
+  {
+    quote: `If the world stopped for bad stuff, then everything would be dark. But the world keeps going so that we can find the crack of light that leads to healing. The only thing people regret is that they didn’t live boldly enough, that they didn’t invest enough heart, didn’t love enough.`,
+    name: "Barbara",
+    role: "GLORI Volunteer",
+  },
+  {
+    file: "ss_04.jpg",
+  },
+  {
+    quote:
+      "The father of a 17 year old with a severe leg injury was so worried his son would end up getting into trouble because his injury kept him from finding a legitimate job. No surgeons in the Dominican Republic had been able to offer his son any options other than a cane.  Our team provided his son with the  surgical intervention needed for recovery. He wanted me to know how thankful he was for us coming there and helping.",
+    name: "Felicia",
+    role: "GLORI Volunteer",
+  },
+  {
+    file: "ss_05.jpg",
+  },
+  {
+    file: "ss_06.jpg",
+  },
+  {
+    quote: `Everytime I go to the DR to do surgeries I want to do more, so I keep organizing trips to go back`,
+    name: "Mark",
+    role: "GLORI Director",
+  },
+  {
+    file: "ss_07.jpg",
+  },
+  {
+    file: "ss_08.jpg",
+  },
+  {
+    quote: `I am proud of the work we did. It was an honor to be a part of the mission. It is important to remember that by pure luck and by no feat of my own that I won some kind of social lottery to have the privileges I have.`,
+    name: "Felicia",
+    role: "GLORI Volunteer",
+  },
+  {
+    file: "ss_09.jpg",
+  },
+  {
+    file: "ss_10.jpg",
+  },
+  {
+    file: "ss_11.jpg",
+  },
+  {
+    file: "ss_12.jpg",
+  },
+  {
+    file: "ss_13.jpg",
+  },
+  {
+    file: "ss_14.jpg",
+  },
+  {
+    file: "ss_15.jpg",
+  },
+];
