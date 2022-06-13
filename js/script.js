1d009ddc006d60546f514437a4ea64ab2a2698e2
Trent Segura <trnt@me.com>
2022-06-13 14:24:25 -0600

    quotes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-const cell = document.querySelector(".carousel-cell");
 const carousel = document.querySelector(".main-carousel");
 
 const generateSlides = (data) => {
@@ -14,39 +13,6 @@ const generateSlides = (data) => {
       img.dataset.original = "./assets/slides/large/" + item.file;
 
       slideDiv.appendChild(img);
-    }
-
-    if (item.quote) {
-      let container = document.createElement("div");
-      container.classList.add("carousel-container");
-      container.classList.add("snippit");
-
-      let para = document.createElement("p");
-      para.classList.add("carousel-quote");
-      para.innerHTML = `"${item.quote}"`;
-
-      let attribute = document.createElement("div");
-
-      let h2 = document.createElement("h2");
-      h2.innerHTML = `${item.name}, `;
-
-      let h3 = document.createElement("h3");
-      h3.innerHTML = item.role;
-
-      let flexbox = document.createElement("div");
-      flexbox.classList.add("flex");
-
-      let link = document.createElement("a");
-      link.href = item.link;
-      link.innerHTML = "Read More...";
-
-      slideDiv.appendChild(container);
-      container.appendChild(para);
-      container.appendChild(flexbox);
-      flexbox.appendChild(attribute);
-      flexbox.appendChild(link);
-      attribute.appendChild(h2);
-      attribute.appendChild(h3);
     }
 
     carousel.appendChild(slideDiv);
@@ -87,3 +53,87 @@ modal.addEventListener("click", (e) => {
     body.style.overflow = "auto";
   }
 });
+
+// Narrative Carousel
+
+const quote_carousel = document.querySelector(".quote-carousel");
+const generateNarratives = (narratives) => {
+  narratives.forEach((narrative, index) => {
+    let narrSlide = document.createElement("div");
+    narrSlide.classList.add("quote-carousel-cell");
+
+    let container = document.createElement("div");
+    container.classList.add("quote-container");
+
+    let para = document.createElement("p");
+    para.classList.add("quote-carousel-para");
+    para.innerHTML = `"${narrative.quote}"`;
+
+    container.appendChild(para);
+
+    narrSlide.appendChild(container);
+    quote_carousel.appendChild(narrSlide);
+  });
+};
+
+generateNarratives(narratives);
+
+var element = document.querySelector(".quote-carousel");
+var flkty2 = new Flickity(element, {
+  // options
+  pageDots: false,
+  wrapAround: true,
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty2 = new Flickity(".quote-carousel", {
+  // options
+});
+
+// const carousel_narrative = document.querySelector(".narrative-carousel");
+// const generateNarratives = (nar) => {
+//   carousel_narrative.innerHTML = "";
+
+//   console.log(nar);
+
+//   nar.forEach((narrative, index) => {
+//     let narSlide = document.createElement("div");
+//     narSlide.classList.add("carousel-cell");
+
+//     let container = document.createElement("div");
+//     container.classList.add("carousel-container");
+//     container.classList.add("snippit");
+
+//     let para = document.createElement("p");
+//     para.classList.add("carousel-quote");
+//     para.innerHTML = `"${narrative.quote}"`;
+
+//     let attribute = document.createElement("div");
+
+//     let h2 = document.createElement("h2");
+//     h2.innerHTML = `${narrative.name}, `;
+
+//     let h3 = document.createElement("h3");
+//     h3.innerHTML = narrative.role;
+
+//     let flexbox = document.createElement("div");
+//     flexbox.classList.add("flex");
+
+//     let link = document.createElement("a");
+//     link.href = narrative.link;
+//     link.innerHTML = "Read More...";
+
+//     narSlide.appendChild(container);
+//     container.appendChild(para);
+//     container.appendChild(flexbox);
+//     flexbox.appendChild(attribute);
+//     flexbox.appendChild(link);
+//     attribute.appendChild(h2);
+//     attribute.appendChild(h3);
+//   });
+
+//   carousel_narrative.appendChild(narSlide);
+// };
+
+// generateNarratives(nar);
---
+++ narrativeData.js
@@ -0,0 +1,79 @@
+narratives = [
+  {
+    quote: `If the world stopped for bad stuff, then everything would be dark. But the world keeps going so that we can find the crack of light that leads to healing. The only thing people regret is that they didn’t live boldly enough, that they didn’t invest enough heart, didn’t love enough.`,
+    name: "Barbara",
+    role: "GLORI Volunteer",
+    link: "barbara.html",
+  },
+  {
+    quote:
+      "The father of a 17 year old with a severe leg injury was so worried his son would end up getting into trouble because his injury kept him from finding a legitimate job. No surgeons in the Dominican Republic had been able to offer his son any options other than a cane.  Our team provided his son with the  surgical intervention needed for recovery. He wanted me to know how thankful he was for us coming there and helping.",
+    name: "Felicia",
+    role: "GLORI volunteer",
+    link: "felicia.html",
+  },
+  {
+    quote:
+      "I am proud of the work we did. It was an honor to be a part of the mission. It is important to remember that by pure luck and by no feat of my own that I won some kind of social lottery to have the privileges I have.",
+    name: "Felicia",
+    role: "GLORI Volunteer",
+    link: "felicia.html",
+  },
+  {
+    quote: `Everytime I go to the DR to do surgeries I want to do more, so I keep organizing trips to go back`,
+    name: "Mark",
+    role: "GLORI Director",
+    link: "mark.html",
+  },
+  {
+    quote: `A great thing that happened in our team is that the roles of surgical tech, surgeon, nurse, anesthetist etc. got blurred and we were just a team doing whatever needed to be done. We all looked out for each other and the hierarchical pressures were erased. We all pitched in to give the best care possible.”`,
+    name: "Felicia",
+    role: "GLORI volunteer",
+    link: "felicia.html",
+  },
+  {
+    quote: `A young female patient tried conversing with me during her anesthesia, attempting to use the google Spanish to English translator. Her response to asking how she was doing, went something like “ don’t cut longer because after that scar you look ugly.” We reassured her that our surgeons were the best!`,
+    name: "Barbara",
+    role: "GLORI Volunteer",
+    link: "barbara.html",
+  },
+  {
+    quote: `The orthopedic surgeries we are performing in the DR are no different than the daily surgeries we do in the States. The difference is that there are plenty of resources here so people can get the care they need easily enough. In the DR, with the vulnerable populations we serve, if we were not there to help, there is nothing.
+    `,
+    name: "Mark",
+    role: "GLORI Co-Director",
+    link: "mark.html",
+  },
+  {
+    quote: `Care for the individual leads to communal good. Most of the patients we see are unable to work due to their injuries. If they can’t work they are a burden on their families because they can’t provide for anyone. This puts strain on the family as well as the community. Never mind the mental health and emotional stress this causes. Our work puts people back to work so they can be contributing members to society.
+    `,
+    name: "Mark",
+    role: "GLORI Co-Director",
+    link: "mark.html",
+  },
+  {
+    quote: `One young man, 17 years old, had a fractured tibia and couldn’t walk. The injury would not heal on its own and he was looking at a long-term disability. He played baseball before the injury and he was very depressed because he couldn’t play anymore.    `,
+    name: "Mark",
+    role: "GLORI Co-Director",
+    link: "mark.html",
+  },
+
+  {
+    quote: `"Several years ago while on a mission trip a 15 year old boy from an orphanage came into the clinic on the last day we were there. He had
+    a club foot. Because it was the last day of our trip we didn’t have
+    time to treat him and this really bothered me. Our last trip in
+    November of 2021 I saw a young man of 18 in the clinic and I knew it
+    was him. I was able to get him into surgery right away. Due to the
+    complicated nature of 18 years of club foot it was an intense
+    surgery and lasted several hours. The team was exhausted but
+    thrilled, it was a successful surgery! I am looking forward to our
+    next visit in April so I can see him for a follow-up and make sure
+    his recovery is going smoothly. If we had better funding we could
+    support the Dominican Orthopedic Surgeons with more thorough
+    follow-up with patients on post surgical treatments."`,
+    name: "Tom",
+    role: "GLORI Co-Director",
+    link: "tom.html",
+    style: "small",
+  },
+];
